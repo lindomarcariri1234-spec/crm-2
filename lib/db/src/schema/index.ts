6e10461aba4080e41926d46106564e2c48f15d8f
cooperturcariri <cooperturcariri@gmail.com>
2026-08-25 00:13:54 -0300

    Sync VisiteCRM platform updates

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -3,6 +3,7 @@ export * from "./users";
 export * from "./clients";
 export * from "./notes";
 export * from "./trips";
+export * from "./trip-imports";
 export * from "./reservations";
 export * from "./payments";
 export * from "./pipeline";
