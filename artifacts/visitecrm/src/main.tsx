0a055031749662f7628a9cb8210497d6b8eb1ad9
lindomarcariri1234-spec <lindomarcariri1234@gmail.com>
2026-08-30 22:03:53 +0000

    Canonicaliza acesso pelo alias da Vercel

--- artifacts/visitecrm/src/main.tsx
+++ artifacts/visitecrm/src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const VERCEL_ALIAS_HOST = "visitecrm.vercel.app";
+const CANONICAL_HOST = "visitecrm.com";
+
+if (window.location.hostname === VERCEL_ALIAS_HOST) {
+  const canonicalUrl = new URL(window.location.href);
+  canonicalUrl.protocol = "https:";
+  canonicalUrl.hostname = CANONICAL_HOST;
+  canonicalUrl.port = "";
+  window.location.replace(canonicalUrl.toString());
+} else {
+  createRoot(document.getElementById("root")!).render(<App />);
+}
